2d843768e5e55492c252cd01dcf5f5debe366e57
razavetisyan <razavetisyan123@gmail.com>
2026-01-28 18:39:13 +0400

    merge-sort

--- sorting-algorithms/merge_sort/mergesort.js
+++ sorting-algorithms/merge_sort/mergesort.js
@@ -48,3 +48,47 @@ function merge(arr, left, mid, right){
     return res;
 }
 console.log(mergeSort(arr, 0, arr.length - 1));
+
+let arr1 = [4,3,6,5,8,7,10,9];
+
+function mergeSort(arr){
+
+    if(arr.length <= 1){
+        return arr;
+    }
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+    let leftSide = mergeSort(left);
+    let rightSide = mergeSort(right);
+
+    return merge(leftSide, rightSide);
+}
+
+function merge(arr1, arr2){
+    let size1 = arr1.length;
+    let size2 = arr2.length;
+    let i = 0;
+    let j = 0;
+    let res = [];
+
+    while(i < size1 && j < size2){
+        if(arr1[i] <= arr2[j]){
+            res.push(arr1[i++]);
+        }else{
+            res.push(arr2[j++]);
+        }
+    }
+
+    while(i < size1){
+        res.push(arr1[i++]);
+    }
+
+    while(j < size2){
+        res.push(arr2[j++]);
+    }
+
+    return res;
+}
+
+console.log(mergeSort(arr1))
